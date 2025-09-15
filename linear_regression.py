b38aed69b19b5ba1ed31c32d2f695f961b017d80
Bryan <byakimisky@gmail.com>
2025-09-15 08:52:24 -0600

    Added multiple linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -2,20 +2,20 @@ import numpy as np
 
 class LinearRegression:
     def __init__(self, alpha : float, n : int):
-        self.w = 0
         self.b = 0
         self.m = 0
         self.training_rate = alpha
         self.iterations = n
     
     def _gradient_descent(self, x : np.ndarray, y : np.ndarray):
-        y_hat = np.dot(x, self.w) + self.b
+        y_hat = np.dot(x, self.w.T) + self.b
         dw = np.dot(x.T, (y_hat - y)) / self.m
         db = np.sum(y_hat - y) / self.m
         return dw, db
     
     def fit(self, x : np.ndarray, y : np.ndarray):
         self.m = x.shape[0]
+        self.w = np.zeros(x.shape[1])
         for i in range(self.iterations):
             dw, db = self._gradient_descent(x,y)
             self.w -= self.training_rate * dw
