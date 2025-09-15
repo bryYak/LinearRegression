fe5007b9987da0b0e5fc9ecc747316f2e8743a79
Bryan <byakimisky@gmail.com>
2025-09-14 18:01:08 -0600

    Added LG in 1D

---
+++ linear_regression.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, alpha : float, n : int):
+        self.w = 0
+        self.b = 0
+        self.m = 0
+        self.training_rate = alpha
+        self.iterations = n
+    
+    def _gradient_descent(self, x : np.ndarray, y : np.ndarray):
+        y_hat = np.dot(x, self.w) + self.b
+        dw = np.dot(x.T, (y_hat - y)) / self.m
+        db = np.sum(y_hat - y) / self.m
+        return dw, db
+    
+    def fit(self, x : np.ndarray, y : np.ndarray):
+        self.m = x.shape[0]
+        for i in range(self.iterations):
+            dw, db = self._gradient_descent(x,y)
+            self.w -= self.training_rate * dw
+            self.b -= self.training_rate * db
+    
+    def get_weights(self):
+        return self.w, self.b
+    
+
+
+
